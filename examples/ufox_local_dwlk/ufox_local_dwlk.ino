e750d22d2ceac4c39cfd3838604d5f8fe31e8d2c
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-09-26 18:01:19 -0500

    lib

---
+++ examples/ufox_local_dwlk/ufox_local_dwlk.ino
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <string.h>
+#include <Ufox.h>
+#include "decode_hex.h"
+
+
+#define btn   13
+#define RXLED  17
+
+Ufox wisol;
+char bufferRx[24];
+uint8_t hexBuffer[12];
+Bits_t data;
+
+
+void setup() {
+  pinMode(btn,INPUT);
+  pinMode(RXLED,OUTPUT);
+  
+  Serial.begin(115200);
+  wisol.begin(9600);
+  
+  while(!Serial);  //comentar si usará una fuente de energía externa
+
+}
+
+void loop() {
+  
+   String rec = wisol.command("AT$RL");    
+   Serial.print("recibido: ");
+   Serial.println(rec);
+
+
+   //limpiar 
+   clean_str_ufox(bufferRx, rec.c_str());                    //se limpia la cadena
+   Serial.print("RX:");
+   Serial.println(bufferRx);
+   convertStringToHEX(hexBuffer, 12, (char*)bufferRx);
+   /*****************************************************/
+   Serial.print("PULSOSA->");
+   Serial.println(GET_UINT32(hexBuffer, BYTE0));
+
+   Serial.print("PULSOS->");
+   Serial.println(GET_UINT32(hexBuffer, BYTE4));
+
+
+   Serial.print("BYTE->");
+   Serial.println(GET_UINT8(hexBuffer, BYTE8));
+   bits_value(&data, GET_UINT8(hexBuffer, BYTE8));
+   Serial.print("REBOT->");
+   
+   Serial.println(data.b7);
+   Serial.print("REFLASH->");
+   Serial.println(data.b6);
+   Serial.print("FRAUDE->");
+   Serial.println(data.b5);
+   
+   Serial.print("CONFIG->");
+   Serial.println(GET_UINT8(hexBuffer, BYTE9));
+   Serial.print("BAT->");
+   Serial.println(GET_UINT16(hexBuffer, BYTE10));
+      
+}
